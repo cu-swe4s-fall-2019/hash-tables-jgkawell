40e78cc0742de250ec7f8582553712a6e3e13cde
Jack Kawell <jgkawell@outlook.com>
2019-10-17 19:40:28 -0600

    Implemented rolling hash

--- hash_functions.py
+++ hash_functions.py
@@ -18,5 +18,21 @@ def h_ascii(key, table_size):
     return -1
 
 
-def h_rolling(key, N):
-    return None
+def h_rolling(key, table_size, p=53, m=2**64):
+    # Check for key as string
+    if isinstance(key, str):
+        try:
+            # Sum up all the ASCII values with a rolling polynomial multiple
+            total = 0
+            for i in range(len(key)):
+                total += ord(key[i]) * p**i
+            total = total % m
+            # Try to return modulo table_size
+            return total % table_size
+        except TypeError:
+            print("ERROR: Must pass a numbers for table_size, p, and m.")
+    else:
+        print("ERROR: Must pass a string as the key to hash function.")
+
+    # Return error code
+    return -1
