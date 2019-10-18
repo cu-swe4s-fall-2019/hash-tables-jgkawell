5eb97b779781b8cb8750f90c320fac8ac130c294
Jack Kawell <jgkawell@outlook.com>
2019-10-17 21:58:19 -0600

    Added unit tests for chained hash

--- test_hash_tables.py
+++ test_hash_tables.py
@@ -9,6 +9,8 @@ import string as s
 
 class TestHashTables(unittest.TestCase):
 
+    """ Test Linear Probe """
+
     def test_linear_probe_ascii_basic(self):
         size = 100
         hash_table = hash_tables.LinearProbe(size, hf.h_ascii)
@@ -63,6 +65,74 @@ class TestHashTables(unittest.TestCase):
     def test_linear_probe_nonexistent_key(self):
         size = 100
         hash_table = hash_tables.LinearProbe(size, hf.h_ascii)
+
+        entries = {}
+        for i in range(int(size / 2)):
+            key = ''.join(r.choices(s.ascii_uppercase + s.digits, k=100))
+            value = ''.join(r.choices(s.ascii_uppercase + s.digits, k=100))
+            entries[key] = value
+            self.assertTrue(hash_table.add(key, value))
+
+        self.assertEqual(
+            hash_table.search(
+                "This is a key that is very unlikely to be generated"), None)
+
+    """ Test Chained Hash """
+
+    def test_chained_hash_ascii_basic(self):
+        size = 100
+        hash_table = hash_tables.ChainedHash(size, hf.h_ascii)
+
+        entries = {}
+        for i in range(int(size / 2)):
+            key = ''.join(r.choices(s.ascii_uppercase + s.digits, k=100))
+            value = ''.join(r.choices(s.ascii_uppercase + s.digits, k=100))
+            entries[key] = value
+            self.assertTrue(hash_table.add(key, value))
+
+        for k, v in entries.items():
+            self.assertEqual(hash_table.search(k), v)
+
+    def test_chained_hash_rolling_basic(self):
+        size = 100
+        hash_table = hash_tables.ChainedHash(size, hf.h_rolling)
+
+        entries = {}
+        for i in range(int(size / 2)):
+            key = ''.join(r.choices(s.ascii_uppercase + s.digits, k=100))
+            value = ''.join(r.choices(s.ascii_uppercase + s.digits, k=100))
+            entries[key] = value
+            self.assertTrue(hash_table.add(key, value))
+
+        for k, v in entries.items():
+            self.assertEqual(hash_table.search(k), v)
+
+    def test_chained_hash_bad_function_name(self):
+        size = 100
+        hash_table = hash_tables.ChainedHash(size, "not function")
+
+        key = ''.join(r.choices(s.ascii_uppercase + s.digits, k=100))
+        value = ''.join(r.choices(s.ascii_uppercase + s.digits, k=100))
+        self.assertFalse(hash_table.add(key, value))
+        self.assertEqual(hash_table.search(key), None)
+
+    def test_chained_hash_bad_size(self):
+        size = "not number"
+        hash_table = hash_tables.ChainedHash(size, hf.h_ascii)
+
+        entries = {}
+        for i in range(500):
+            key = ''.join(r.choices(s.ascii_uppercase + s.digits, k=100))
+            value = ''.join(r.choices(s.ascii_uppercase + s.digits, k=100))
+            entries[key] = value
+            self.assertTrue(hash_table.add(key, value))
+
+        for k, v in entries.items():
+            self.assertEqual(hash_table.search(k), v)
+
+    def test_chained_hash_nonexistent_key(self):
+        size = 100
+        hash_table = hash_tables.ChainedHash(size, hf.h_ascii)
 
         entries = {}
         for i in range(int(size / 2)):
